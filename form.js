0fb770bd6df0e267668b5d1fbff0f611e7d751ca
hs331614 <hs331614@gmail.com>
2023-05-27 09:32:25 +0530

    new

--- form.js
+++ form.js
@@ -53,3 +53,24 @@ const saveMessages = (name, email, phone,occupation) => {
 const getElementVal = (id) => {
   return document.getElementById(id).value;
 };
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal
+var img = document.querySelector(".poster");
+var modalImg = document.getElementById("img01");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
