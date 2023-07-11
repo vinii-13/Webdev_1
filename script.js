65ea27700c6c3310835a01627672af52c78d44d6
Vartika Sharma <vertikasharma21112@gmail.com>
2023-07-11 23:13:32 +0530

    ..

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-/*
- var swiper = new Swiper(".mySwiper", {
-=======
 
  /* var swiper = new Swiper(".mySwiper", {
->>>>>>> Stashed changes
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -17,15 +12,4 @@
     prevEl: ".swiper-button-prev",
   },
 });
-<<<<<<< Updated upstream
 */
-function videoslider(links){
-  document.querySelector(".slider").src=links;
-}
-window.addEventListener('load', () => {
-  const preloader = document.getElementById('preloader');
-  preloader.classList.add('hide-preloader');
-});
-=======
-*/
->>>>>>> Stashed changes
